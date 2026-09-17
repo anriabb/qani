1ba72942ec8b670d5eefb0df0f064eefa8a88407
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 13:34:12 +0400

    Update CORS allowed origins in WebConfig

--- qani_back/src/main/java/backend/demo/config/WebConfig.java
+++ qani_back/src/main/java/backend/demo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("https://qani-omzn.onrender.com")
+                .allowedOriginPatterns("https://*.onrender.com", "http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
