b0e34cf5db3c473339848f1ec718d740e29f269d
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 11:47:08 +0400

    Add HomeController

---
+++ qani_back/src/main/java/backend/demo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package backend.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Qani Backend is up and running successfully!";
+    }
+}
