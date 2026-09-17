de7058919c3f2dc4156d8189c33f82f87a690645
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 13:48:30 +0400

    Update HeaderScannerController.java

--- qani_back/src/main/java/backend/demo/controllers/HeaderScannerController.java
+++ qani_back/src/main/java/backend/demo/controllers/HeaderScannerController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/headers")
 @Tag(name = "Email Header Security API", description = "Analyzes email authentication headers for spoofing risks")
