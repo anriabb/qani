05ce4ff58f445191af95c4f0165eb8af522aa10d
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 13:41:59 +0400

    Remove CrossOrigin annotation from PasswordGeneratorController
    Removed CrossOrigin annotation to restrict origins.

--- qani_back/src/main/java/backend/demo/controllers/PasswordGeneratorController.java
+++ qani_back/src/main/java/backend/demo/controllers/PasswordGeneratorController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/passwords")
 @Tag(name = "Password Generator API", description = "Generates high-entropy passwords")
