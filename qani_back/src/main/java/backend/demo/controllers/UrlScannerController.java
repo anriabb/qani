7456f55e5dcd1e1339db42b1a1f45030695e326c
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 13:47:04 +0400

    Update UrlScannerController.java

--- qani_back/src/main/java/backend/demo/controllers/UrlScannerController.java
+++ qani_back/src/main/java/backend/demo/controllers/UrlScannerController.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
-
 @RestController
 @RequestMapping("/api/v1/urls")
 @Tag(name = "URL Phishing API", description = "Endpoints for analyzing link phishing risks")
