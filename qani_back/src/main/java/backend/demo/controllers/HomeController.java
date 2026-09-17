009304b7f36e8eab6a527819e6671561c08cc91c
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 11:49:04 +0400

    Refactor HomeController to use Controller annotation
    Changed HomeController from RestController to Controller and updated home method return value.

--- qani_back/src/main/java/backend/demo/controllers/HomeController.java
+++ qani_back/src/main/java/backend/demo/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package backend.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Qani Backend is up and running successfully!";
+        return "index"; 
     }
 }
