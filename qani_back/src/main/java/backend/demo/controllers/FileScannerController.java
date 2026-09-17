eadbc66c6b39d1482ee0d1535e0460a27e8d7ba3
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 13:48:42 +0400

    Update FileScannerController.java

--- qani_back/src/main/java/backend/demo/controllers/FileScannerController.java
+++ qani_back/src/main/java/backend/demo/controllers/FileScannerController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/files")
 @Tag(name = "File Scanner API", description = "Endpoints for file threat analysis")
