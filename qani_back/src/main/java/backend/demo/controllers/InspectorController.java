6f9ca4beb805b5a81a307c1b496c3988838534fe
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 13:48:15 +0400

    Remove CrossOrigin annotation from InspectorController

--- qani_back/src/main/java/backend/demo/controllers/InspectorController.java
+++ qani_back/src/main/java/backend/demo/controllers/InspectorController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/inspector")
 @Tag(name = "IP & URL Inspector API", description = "Network geolocation, ISP lookup, and URL security inspection endpoints")
