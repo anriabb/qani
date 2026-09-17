e21a425330dff44927e71f27caacb8602bf89434
Anri Abuladze <149783052+anriabb@users.noreply.github.com>
2026-09-17 13:10:30 +0400

    Update WebConfig.java

--- qani_back/src/main/java/backend/demo/config/WebConfig.java
+++ qani_back/src/main/java/backend/demo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://qani-omzn.onrender.com")
+                .allowedOriginPatterns("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
